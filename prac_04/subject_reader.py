6300e8fe8708096599759c63d5f2d88638d96396
WangXiang-815 <1512468411@qq.com>
2025-02-23 15:38:40 +0800

    Refactor subject_reader.py to process and display subject details

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,11 +8,12 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = load_data()
-    print(data)
+    display_subjects_details(data)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data=[]   #create a list to store parts.
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -22,8 +23,14 @@ def load_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        data.append(parts) #Add parts to the list.
         print("----------")
     input_file.close()
+    return data
 
+def display_subjects_details(data):
+    """Display subjects details in the required format,EG:CP1401 is taught by Ada Lovelace and has 192 students"""
+    for subjects_details in data:
+        print(f"{subjects_details[0]} is taught by {subjects_details[1]} and has {subjects_details[2]} students")
 
 main()
