ff7e31728d8bccc8bad1ac3129b09509f2aa87b7
WangXiang-815 <1512468411@qq.com>
2025-02-23 15:55:50 +0800

    Add usernames authentication to list_exercises.py

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,5 +1,6 @@
 numbers = [] #create list for further storing the numbers input
-
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 #Ask the user to input the number for 5 times
 for i in range(5):
     number = int(input("Number: "))
@@ -11,3 +12,12 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average number is {sum(numbers) / len(numbers)}")
+
+#Authority checker
+user_input = input("Enter your name: ")
+
+#Check if input is authorized
+if user_input in usernames:
+    print("Access Granted!")
+else:
+    print("Access Denied!")
