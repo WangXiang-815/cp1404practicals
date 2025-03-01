0a909eba45614e28730997720762cc354a44fa13
wangxiang-815 <1512468411@qq.com>
2025-03-01 14:24:55 +0800

    add main func add 3 TODO

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,3 +2,15 @@
 estimated time: 15mins
 actual time:
 """
+
+FILENAME = 'wimbledon.csv'
+
+
+def main():
+    """read csv data and print wimbledon champions and countries details"""
+    records = load_record(FILENAME) #TODO:write load_record func
+    champion_to_count, countries = process_record(records) #TODO:write process record func
+    display_records(champion_to_count, countries)#TODO:write display func
+
+def load_record(FILENAME):
+    """load data and create a list of lists to store"""
