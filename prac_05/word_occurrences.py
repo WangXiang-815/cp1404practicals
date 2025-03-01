5790d44134d39723a07b7484a85bcf5bca9d18eb
wangxiang-815 <1512468411@qq.com>
2025-03-01 13:53:40 +0800

    using LBYL to complete word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,1 +1,24 @@
 #estimated time:15 mins
+
+word_to_count = {} #create a dict to store words and count
+text = input('Text: ') #ask input
+
+#split the text into words
+words = text.split()
+
+# using LBYL pattern to count
+for word in words:
+    word_to_count[word] = word_to_count.get(word,0) + 1
+
+#sort dict by alphabetical order
+sorted_word_count = sorted(word_to_count.items())
+
+#find the longest word
+max_length = max((len(word) for word in words))
+
+#print in the required formating
+for word, count in sorted_word_count:
+    print(f"{word:{max_length}} : {count}")
+
+
+
