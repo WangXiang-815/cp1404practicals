c894e050fc36b317365d4f8b8c7c65744e48b66f
wangxiang-815 <1512468411@qq.com>
2025-03-01 14:06:10 +0800

    complete main func

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,3 +3,23 @@ estimated time: 20mins
 actual time:
 """
 
+def main():
+    """create dict of emails_to_names"""
+    email_to_name ={}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email) #TODO: write this func
+        confirmation = input(f"Is your name {name}? (Y/n)")
+        #press Enter to accept the default of (Y)es.
+        if confirmation.upper() != "Y" and confirmation != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email =input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} {email}")
+
+def get_name_from_email(email):
+
+
+
