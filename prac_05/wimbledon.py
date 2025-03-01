531882d511c37389fd428c5178ba94173bf7d1fe
wangxiang-815 <1512468411@qq.com>
2025-03-01 14:31:29 +0800

    add  process record func remove 1 TODO add two constant variable

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,7 +4,8 @@ actual time:
 """
 
 FILENAME = 'wimbledon.csv'
-
+INDEX_COUNTRIES = 1
+INDEX_CHAMPIONS = 2
 
 def main():
     """read csv data and print wimbledon champions and countries details"""
@@ -14,12 +15,26 @@ def main():
 
 def load_record(FILENAME):
     """load data and create a list of lists to store"""
-    record = []
+    records = []
     with open(FILENAME,'r', encoding="utf-8-sig") as file:
         file.readline() #remove header
         for line in file:
             parts = line.strip().split()
-            record.append(parts) #split and store to record list
+            records.append(parts) #split and store to record list
 
-    return record
+    return records
+
+def process_record(records):
+    """create a dict to store champions and set of countries from list of lists(records)"""
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[INDEX_COUNTRIES])
+        try:
+            champion_to_count[record[INDEX_CHAMPIONS]] += 1
+
+        except KeyError:
+            champion_to_count[record[INDEX_CHAMPIONS]] = 1
+
+    return champion_to_count, countries
 
