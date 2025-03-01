b7ca9f635633ae9ea1f005c8e010dcced375b72f
wangxiang-815 <1512468411@qq.com>
2025-03-01 13:38:00 +0800

    complete hex_colours.py

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,7 +1,18 @@
 # A constant dictionary with 10 color names and their corresponding hexadecimal codes
 COLOR_NAME_TO_HEX = {
-    "AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "Aqua": "#00ffff", "Azure": "#f0ffff",
-    "Beige": "#f5f5dc", "Bisque": "#ffe4c4", "Black": "#000000", "BlanchedAlmond": "#ffebcd",
-    "Blue": "#0000ff", "BlueViolet": "#8a2be2"
+    "aliceBlue": "#f0f8ff", "antiqueWhite": "#faebd7", "aqua": "#00ffff", "azure": "#f0ffff",
+    "beige": "#f5f5dc", "bisque": "#ffe4c4", "black": "#000000", "blanchedAlmond": "#ffebcd",
+    "blue": "#0000ff", "blueViolet": "#8a2be2"
 }
 
+# Loop to get color name input and return the corresponding hexadecimal code
+colour_name = input("Enter a colour name: ").lower()  # Convert input to lowercase
+while colour_name != "":
+    hex_code = COLOR_NAME_TO_HEX.get(colour_name)
+    if hex_code:
+        print(f"The code for \"{colour_name.title()}\" is {hex_code}")
+    else:
+        print(f"Invalid colour name: \"{colour_name.title()}\"")
+
+    # Ask the user to input another colour name or blank to stop
+    colour_name = input("Enter a colour name: ").lower()  # Convert input to lowercase
