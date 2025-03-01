374f5af6967d51aff9ea0db95249f5509b0b54da
wangxiang-815 <1512468411@qq.com>
2025-03-01 13:33:41 +0800

    create a dictionary with color names and hex codes

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,7 @@
+# A constant dictionary with 10 color names and their corresponding hexadecimal codes
+COLOR_NAME_TO_HEX = {
+    "AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "Aqua": "#00ffff", "Azure": "#f0ffff",
+    "Beige": "#f5f5dc", "Bisque": "#ffe4c4", "Black": "#000000", "BlanchedAlmond": "#ffebcd",
+    "Blue": "#0000ff", "BlueViolet": "#8a2be2"
+}
+
