d2b2f3748042ba35bcfabad2850b46fb43e50dcc
wangxiang-815 <1512468411@qq.com>
2025-03-01 14:27:09 +0800

    add load record func remove 1 TODO

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -8,9 +8,18 @@ FILENAME = 'wimbledon.csv'
 
 def main():
     """read csv data and print wimbledon champions and countries details"""
-    records = load_record(FILENAME) #TODO:write load_record func
+    records = load_record(FILENAME)
     champion_to_count, countries = process_record(records) #TODO:write process record func
     display_records(champion_to_count, countries)#TODO:write display func
 
 def load_record(FILENAME):
     """load data and create a list of lists to store"""
+    record = []
+    with open(FILENAME,'r', encoding="utf-8-sig") as file:
+        file.readline() #remove header
+        for line in file:
+            parts = line.strip().split()
+            record.append(parts) #split and store to record list
+
+    return record
+
