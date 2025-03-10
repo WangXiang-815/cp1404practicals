1759b47b32f2d4286e583df7d05deda80ce680a9
wangxiang-815 <1512468411@qq.com>
2025-03-10 14:07:13 +0800

    Define __init__ method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,3 +4,15 @@ Start time: 13:59 10th March
 Finish time:
 Actual time:
 """
+
+
+class ProgrammingLanguage:
+
+    def __init__(self, name,typing, reflection, year):
+        """Construct a ProgrammingLanguage in value listed in readme.md"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
