9b56e6913d93d586eaebf682f3c0546f231c6b17
wangxiang-815 <1512468411@qq.com>
2025-03-10 13:54:06 +0800

    Complete modifying the codes

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -2,6 +2,8 @@
 CP1404/CP5632 Practical - Client code to use the Car class.
 Note that the import has a folder (module) in it.
 This is why we name our folders with no spaces or capitals, as valid module names.
+EST: 15mins
+ACT: 14mins
 """
 
 from prac_06.car import Car
@@ -15,13 +17,13 @@ def main():
     print(my_car)
 
     #create a new car object named 'Limo' with 100 fuel.
-    limo = Car(100)
+    limo = Car('Limo',100)
     #add 20 fuel to limo
     limo.add_fuel(20)
     #print the current fuel of Limo
     print(limo.fuel)
     #drive limo 115km using the drive method
     limo.drive(115)
-
+    print(limo)
 
 main()
