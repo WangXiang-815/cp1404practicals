dd8fd8c5c9b48b536e760e58b7eb31eaa3e85e93
wangxiang-815 <1512468411@qq.com>
2025-03-10 14:23:19 +0800

    Add import class line Copied the required lines

---
+++ prac_06/languages.py
@@ -0,0 +1,13 @@
+"""
+languages.py
+client code
+"""
+
+#import class ProgrammingLanguage
+from programming_language import ProgrammingLanguage
+
+#these 4 lines are copied from readme.md :)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
