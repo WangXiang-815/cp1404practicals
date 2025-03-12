74c753bc0d726c9686eb25d9ae84e8292de850ee
wangxiang-815 <1512468411@qq.com>
2025-03-12 15:56:56 +0800

    Complete guitar_test.py

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,23 @@
+"""
+manual test for Guitar
+"""
+from prac_06.guitar import Guitar
+
+
+def run_tests():
+    "Test Guitar's is_vintage and get_age func"
+    current_year = 2025
+    guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    guitar2 = Guitar("Another Guitar", 2013, 1512.90)
+
+    #Test get_age()
+    print(f"{guitar1.name} get_age() - Expected 103. Got {guitar1.get_age()}")
+    print(f"{guitar2.name} get_age() - Expected 103. Got {guitar2.get_age()}")
+
+    #Test is_vintage()
+    print(f"{guitar1.name} is_vintage() - Expected 103. Got {guitar1.is_vintage()}")
+    print(f"{guitar2.name} is_vintage() - Expected 103. Got {guitar2.is_vintage()}")
+
+#Call run_test func
+if __name__ == '__main__':
+    run_tests()
