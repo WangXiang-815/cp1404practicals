4d83ec69fe5f3e11dde80e0b141d1bdce45b5b45
wangxiang-815 <1512468411@qq.com>
2025-03-12 15:23:00 +0800

    Add get_age, is_vintage

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -5,6 +5,7 @@ Actual time:
 
 class Guitar:
 
+
     def __init__(self, name, year, cost):
         "Initialize Guitar"
         self.name = name
@@ -15,4 +16,11 @@ class Guitar:
         "Return a string representation of Guitar"
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
+    def get_age(self):
+        "Return a guitar's age"
+        current_year = 2025
+        return current_year - self.year
 
+    def is_vintage(self):
+        "Return True if guitar is over 50 years old"
+        return self.get_age() >= 50
