451af5260594a18d3c2001c68570047f4be658e6
wangxiang-815 <1512468411@qq.com>
2025-03-23 14:45:21 +0800

    Add __lt__ method to solve TypeError

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -30,3 +30,7 @@ class Guitar:
     def is_vintage(self):
         "Return True if guitar is over 50 years old"
         return self.get_age() >= 50
+
+    def __lt__(self, other):
+        """Less than comparison based on year"""
+        return self.year < other.year
