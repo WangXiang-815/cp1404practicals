94f81561f2a633c789f30e1e76b4167775d0b01e
wangxiang-815 <1512468411@qq.com>
2025-03-26 17:59:45 +0800

    Created basic app structure with Kivy Set up window size and title Linked app to corresponding KV file for layout

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -4,3 +4,20 @@ ACT:
 python codes for converting miles to kilometers
 """
 
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class ConvertMiles(App):
+    """ConvertMiles is kivy app for converting miles to kilometers"""
+    def build(self):
+        """build the Kivy app from the kv file"""
+        Window.size = (300,200)
+        self.title = 'Convert Miles to Kilometers'
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+
+
+ConvertMiles().run()
