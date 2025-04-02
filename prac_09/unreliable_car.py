a0d29fe470bcf395fdd105d33f7d5a6f5f5abba7
wangxiang-815 <1512468411@qq.com>
2025-04-02 15:24:28 +0800

    Add __init__ Add drive method with reliability

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,2 +1,27 @@
 import random
+from random import randint
 from car import Car
+
+class UnreliableCar(Car):
+    """A special car that does not drive reliably"""
+
+    def __init__(self,name, fuel, reliability):
+        """Initialize an UnreliableCar with a random reliability percentage
+        Args:
+            name(str): name of car
+            fuel(int): fuel of car
+            reliability(int) : 0-100 percentage
+        """
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive car only when the random number is less than the car's reliability"""
+        #generate random_number
+        random_number = randint(0,100)
+        if random_number >= self.reliability:
+            distance = 0
+
+        #Call parent Class's drive method
+        drive_distance = super().drive(distance)
+        return drive_distance
