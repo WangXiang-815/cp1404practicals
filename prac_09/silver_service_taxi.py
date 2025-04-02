b1f27b9e6fe2271b0940808439e28e3bd8e6eca9
wangxiang-815 <1512468411@qq.com>
2025-04-02 16:19:48 +0800

    Add __init__

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,16 @@
+from taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    """A specialized Taxi with a higher fare based on fanciness."""
+    flagfall = 4.5
+
+    def __init__(self, name, fuel, fanciness):
+        """Initialize a SilverServiceTaxi"""
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def __str__(self):
+        """Return a string representation of a SilverServiceTaxi"""
+
+
