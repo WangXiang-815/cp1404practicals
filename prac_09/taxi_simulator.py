a495a4dd18421bda0eb6884a2183e99efac6eead
wangxiang-815 <1512468411@qq.com>
2025-04-06 10:58:20 +0800

    add part of main() add display_taxis()

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -8,3 +8,25 @@ from car import Car
 from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
 
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+def main():
+    """A taxi simulator program matches readme.md's requirements"""
+    total_bill = 0
+    current_taxi = None
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo",100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    print("Let's drive!")
+    print(MENU)
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "c":
+            print("Taxis available: ")
+            display_taxis(taxis)
+            taxis_choice = input("Choose taxi: ")
+        elif choice == "d":
+
+def display_taxis(taxis):
+    """Display taxis in specified format"""
+    for i,taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+
