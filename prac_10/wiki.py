a1dde584c4faad5760e25357c6587da5072f9df1
wangxiang-815 <1512468411@qq.com>
2025-04-12 07:50:07 +0800

    Implement for wiki.py

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,9 +1,24 @@
 import wikipedia
+from wikipedia.exceptions import DisambiguationError, PageError
+
 
 def main():
     """A function to try wikipedia"""
     while True:
         title = input("Enter title page:").strip()
-        if title == '':
+        if not title:
             print("Thank you.")
             break
+
+    try:
+        page = wikipedia.page(title, auto_suggest=False)
+        print(page.title)
+        print(page.summary)
+        print(page.url)
+    except DisambiguationError:
+        print("We need a more specific title. Try one of the following, or a new search:")
+        print(DisambiguationError.options[:5],"...")
+    except PageError:
+        print(f"Page id {title} does not match any pages. Try another id!")
+
+main()
